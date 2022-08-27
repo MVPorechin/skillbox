e4f2f21410a291fa017a06bf06242a4aef8a3f8b
Maksim Porechin <m.porechin@ya.ru>
2022-08-27 11:30:46 +0300

    edit draft. all task without comments. pure code with print

--- draft.py
+++ draft.py
@@ -1,9 +1,34 @@
-print('14.3 Командная строка и интерпретатор')
+print('Задача 1. Таблица умножения: возвращение')
 
-for i in range(1, 11):
-    for j in range(1, 9):
+for i in range(1, 10):
+    for j in range(1, 10):
         print(i * j, end='\t')
     print()
+
+print('Задача 2. Калькулятор')
+
+while True:
+    command = input("Выберите операцию: ")
+    if command in "+-*/":
+        break
+    print("Ошибка: такой операции не существует. Попробуйте ещё раз.")
+
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+
+result = 0
+if command == "+":
+    result = first + second
+elif command == "-":
+    result = first - second
+elif command == "*":
+    result = first * second
+elif command == "/":
+    result = first / second
+
+print(f"{first} {command} {second} = {result}")
+
+print('Задача 3. Калькулятор 2')
 
 while True:
     command = input("Выберите операцию: ")
@@ -30,4 +55,3 @@ while number != 0:
     result_str += " " + command + " " + str(number)
 
 print(result_str + " = " + str(result))
-

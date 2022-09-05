329f106bb52a368613412272dc7c123cc8ed3afd
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2022-09-05 16:55:05 +0300

    add draft_m14

---
+++ draft_m14.py
@@ -0,0 +1,27 @@
+# print('m14 Задача 1. Таблица степеней')
+# numbers = [3, 7, 5]
+# while True:
+#     number = int(input('Новое число: '))
+#     numbers.append(number)
+#     print('Текущий список чисел:', numbers)
+#     for _ in numbers:
+#         print(_ ** 2, _ ** 3, _ ** 4)
+# print()
+
+# print('m14 Задача 1. Очень простая задача')
+# list = []
+# for _ in range(0,101):
+#     list.append(_)
+# print(list)
+
+# print('m14 Задача 3. Контроль')
+# count_personal = int(input('Введите кол-во сотрудников: '))
+# list_id = []
+# for _ in range(count_personal):
+#     id_personal = int(input('ID сотрудника: '))
+#     list_id.append(id_personal)
+# search_id = int(input('Какой ID ищем?: '))
+# if search_id not in list_id:
+#     print(f'Сотрудник {search_id} не работает!')
+# else:
+#     print('Все сотрудники на работе')

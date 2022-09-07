22afa8a3b47078a80e74f5423fc70716973245f2
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2022-09-07 15:21:41 +0300

    add somo task in draft_m14

--- draft_m14.py
+++ draft_m14.py
@@ -25,3 +25,48 @@
 #     print(f'Сотрудник {search_id} не работает!')
 # else:
 #     print('Все сотрудники на работе')
+
+# print('m14 Задача 1. Гугл')
+# nums_list = []
+# N = int(input('Кол-во чисел в списке: '))
+#
+# for _ in range(N):
+#     num = int(input('Очередное число: '))
+#     nums_list.append(num)
+#
+# if nums_list:
+#     maximum = nums_list[0]
+#     minimum = nums_list[0]
+#     for i in nums_list:
+#         if maximum < i:
+#             maximum = i
+#         elif minimum > i:
+#             minimum = i
+#
+# print(f'Максимальное число в списке: {maximum}')
+# print(f'Минимальное число в списке: {minimum}')
+
+print('m14 Задача  2. Кратность')
+nums_list = []
+N = int(input('Кол-во чисел в списке: '))
+
+for i in range(1, N + 1):
+    num = int(input(f'Введите {i} число: '))
+    nums_list.append(num)
+
+divider = int(input('Введите делитель: '))
+index = 0
+sum_indexes = 0
+for number in nums_list:
+    if number % divider == 0:
+        print(f"Индекс числа {number}: {index}")
+        sum_indexes += index
+    index += 1
+
+print("Сумма индексов:", sum_indexes)
+
+sum_indexes = 0
+for index, number in enumerate(nums_list):  # enumerate в таких случаях очень полезен
+    if number % divider == 0:
+        print(f"Индекс числа {number}: {index}")
+        sum_indexes += index

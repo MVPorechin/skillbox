c285847de197dd091aabb83d3bfb1b3d3a798d5b
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2022-10-02 00:17:02 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	draft_m15.py

--- draft_m15.py
+++ draft_m15.py
@@ -51,3 +51,64 @@
 #     print('Минимальное число в списке:', minimum)
 # else:
 #     print('В списке нет чисел')
+
+print('15.Задача 2.Задача 2. Кратность')
+nums_list = []
+
+N = int(input('Кол-во чисел в списке: '))
+
+for i in range(1, N + 1):
+    num = int(input(f'Введите {i} число: '))
+    nums_list.append(num)
+
+divider = int(input('Введите делитель: '))
+index = 0
+sum_indexes = 0
+for number in nums_list:
+    if number % divider == 0:
+        print(f"Индекс числа {number}: {index}")
+        sum_indexes += index
+    index += 1
+
+print("Сумма индексов:", sum_indexes)
+
+sum_indexes = 0
+for index, number in enumerate(nums_list):  # enumerate в таких случаях очень полезен
+    if number % divider == 0:
+        print(f"Индекс числа {number}: {index}")
+        sum_indexes += index
+
+print('15.Задача 2.Задача 3. Собачьи бега')
+
+nums_list = []
+
+N = int(input('Кол-во чисел в списке: '))
+
+for _ in range(N):
+    num = int(input('Очередное число: '))
+    nums_list.append(num)
+
+if nums_list:
+    maximum = nums_list[0]
+    minimum = nums_list[0]
+
+    minimum_index = 0
+    maximum_index = 0
+    for index, i in enumerate(nums_list):
+
+        if maximum < i:
+            maximum = i
+            maximum_index = index
+
+        if minimum > i:
+            minimum = i
+            minimum_index = index
+
+    print('Максимальное число в списке:', maximum)
+    print('Минимальное число в списке:', minimum)
+
+    print(nums_list)
+    nums_list[minimum_index], nums_list[maximum_index] = nums_list[maximum_index], nums_list[minimum_index]
+    print(nums_list)
+else:
+    print('В списке нету чисел')

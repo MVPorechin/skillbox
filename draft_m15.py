dac2753a03afc6020e99863e445a53912158715a
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2022-10-01 22:24:11 +0300

    add draft module 15

---
+++ draft_m15.py
@@ -0,0 +1,32 @@
+# print('15.Задача 1. Таблица степеней')
+# numbers = [3,7,5]
+#
+# while True:
+#     number = int(input('Новое число: '))
+#     numbers.append(number)
+#     print('Текущий список чисел:', numbers)
+#
+# for _ in number:
+#     print(i ** 2, i ** 3, i ** 4)
+#     print()
+
+# print('15.Задача 2. Очень простая задача')
+# list_number = []
+# for _ in range(101):
+#     list_number.append(_)
+#
+# print(list_number)
+
+# print('15.Задача 3. Контроль')
+# count_personal = int(input('Кол-во сотрудников в офисе: '))
+# list_id = []
+# for _ in range(count_personal):
+#     id = int(input('ID сотрудника: '))
+#     list_id.append(id)
+#
+# find_id_personal = int(input('Какой ID ищем?: '))
+#
+# if find_id_personal not in list_id:
+#     print('Сотрудник не работает')
+# else:
+#     print('Сотрудник работает')

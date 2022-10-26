f36f313f23b3b067961481b6607b8129375919c7
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2022-10-27 01:02:20 +0300

    initial commit

--- draft_m17.py
+++ draft_m17.py
@@ -34,30 +34,50 @@
 # print(f'{word_list}')
 # print(f'{word_list_special}')
 
-print('17.1 - Задача 3. Повышение цен')
+# print('17.1 - Задача 3. Повышение цен')
 
 # Дан список цен на пять товаров с точностью до копейки. Так как экономика даёт о себе знать, мы спрогнозировали, что через год придётся повышать цены на X процентов, а ещё через один год — ещё на Y процентов.
 # Напишите программу, которая получает на вход список цен на товары (вещественные числа, список генерируется также с помощью list comprehensions) и выводит в одну строку общую сумму стоимости товаров за каждый год.
 
-prices = [float(input('Цена за товар :')) for _ in range(5)]
-first_year_percent = int(input('Повышение на первый год: '))
-second_year_percent = int(input('Повышение на второй год: '))
-
-result_prices = 'Сумма цен за каждый год: '
-for percent in 0, first_year_percent, second_year_percent:
-    prices = [price * (1 + percent / 100) for price in prices]
-    result_prices += f'{round(sum(prices), 2)}'
-
-
-print(result_prices)
-# prices = [float(input("Цена на товар: ")) for _ in range(5)]
+# prices = [float(input('Цена за товар :')) for _ in range(5)]
+# first_year_percent = int(input('Повышение на первый год: '))
+# second_year_percent = int(input('Повышение на второй год: '))
 #
-# first_year = int(input("Повышение на первый год: "))
-# second_year = int(input("Повышение на второй год: "))
-#
-# all_prices = "Сумма цен за каждый год: "
-# for percent in 0, first_year, second_year:
+# result_prices = []
+# for percent in 0, first_year_percent, second_year_percent:
 #     prices = [price * (1 + percent / 100) for price in prices]
-#     all_prices += f" {round(sum(prices), 2)}"
+#     result_prices.append(round(sum(prices), 2))
 #
-# print(all_prices)
+# print(f'Сумма цен за каждый год: {result_prices}')
+
+
+# squares_odds = [x ** 2 for x in range(10) if x % 2 != 0]
+# squares_cubes = [(x ** 2 if x % 2 != 0 else x ** 3) for x in range(10)]
+#
+# print(f'{squares_odds}\n')
+# print(f'{squares_cubes}\n')
+#
+import random
+#
+# squad_1 = [random.randint(50, 80) for _ in range(10)]
+# squad_2 = [random.randint(30, 60) for _ in range(10)]
+# squad_3_condition = [('Погиб' if squad_1[i_damage] + squad_2[i_damage] > 100
+#                       else 'Выжил')
+#                      for i_damage in range(10)]
+#
+# print(f'Урон от первого отряда {squad_1}')
+# print(f'Урон от первого отряда {squad_2}')
+# print(f'Урон от первого отряда {squad_3_condition}')
+
+# print('17.2 - Задача 1. Список чётных чисел')
+# # Пользователь вводит два числа: А и В. Реализуйте код, который генерирует список из чётных чисел в диапазоне от А до B. Используйте list comprehensions (как и в следующих задачах).
+# numbers_a = int(input('Введите число А: '))
+# numbers_b = int(input('Введите число В: '))
+# condition = [random.randint(numbers_a, numbers_b) % 2 == 0 for _ in range(numbers_a, numbers_b +1)]
+# # result = [condition[_] % 2 == 0 for _ in range(numbers_a, numbers_b)]
+#
+# print(f'Результат от {numbers_a} до {numbers_b} : {condition}')
+
+original_prices = [1.25, -9.45, 10.22, 3.78, -5.92, 1.16]
+new_prices = [price if price > 0 else 0 for price in original_prices]
+print("Результат:\n\n", new_prices, sep="")

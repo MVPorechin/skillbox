c43d253a809e905c4c798c8e8e0caf491c39adf7
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2022-11-19 23:56:49 +0300

    initial commit

--- draft_m19.py
+++ draft_m19.py
@@ -1,3 +1,4 @@
+import random
 # phonebook_dict = {
 #     'Ваня': 88006664545,
 #     'Петя': 88003332233,
@@ -286,3 +287,29 @@
 #     if elem in symbols:
 #         count += 1
 # print("Количество знаков пунктуации:", count)
+# print('Задача 2. Семинар')
+# nums_1 = [29, 17, 10, 15, 13, 22, 12, 22, 7, 24, 26, 3, 11, 2, 3, 16, 19, 21, 2, 3, 8, 27, 2, 17, 2, 20, 12, 21, 3, 1]
+# nums_2 = [16, 21, 30, 24, 5, 7, 23, 13, 11, 5, 21, 5, 19, 9, 12, 9, 15, 16, 29, 8, 16, 1, 22, 15, 16, 9, 1, 13, 21, 21]
+# nums_set_1 = set(nums_1)
+# nums_set_2 = set(nums_2)
+# print(f'1-е множество: {nums_set_1}')
+# print(f'2-е множество: {nums_set_2}')
+# print(f'Минимальный элемент 1-го множества: {min(nums_set_1)}')
+# print(f'Минимальный элемент 2-го множества: {min(nums_set_2)}')
+# nums_set_1.discard(min(nums_set_1))
+# nums_set_2.discard(min(nums_set_2))
+# rand_num_1 = random.randint(100, 200)
+# rand_num_2 = random.randint(100, 200)
+# nums_set_1.add(rand_num_1)
+# nums_set_2.add(rand_num_2)
+# print(f'Случайное число для 1-го множества: {rand_num_1}')
+# print(f'Случайное число для 2-го множества: {rand_num_2}')
+# print(f'Объединение множеств: {nums_set_1 | nums_set_2}')
+# print(f'Пересечение множеств: {nums_set_1 & nums_set_2}')
+# print('Задача 3. Различные цифры')
+#
+# # text = input('Введите текст: ')
+# text = 'ab1n32kz2'
+# text_set = set(text)
+# result = text_set & set("0123456789")
+# print(''.join(result))

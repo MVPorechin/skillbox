13b7877cc6455cb78030ee918f41b3942fc07de7
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-02-05 14:47:00 +0300

    draft m20_v0.03

--- draft_m21.py
+++ draft_m21.py
@@ -1,3 +1,4 @@
+import random
 ## home work
 # def shortest_seq_range(string, tpl):
 #     return min(len(string), len(tpl))
@@ -114,6 +115,8 @@
 # ней и найти нужный элемент. Для этого в программировании используются специальные алгоритмы поиска.
 # Напишите функцию, которая находит заданный пользователем ключ в словаре и выдаёт значение этого ключа на экран. В
 # качестве примера можно использовать такой словарь:
+
+
 # def search_element(data, tag):
 #     result = None
 #     if tag in data:
@@ -124,7 +127,7 @@
 #             if result:
 #                 return result
 #     return result
-#
+
 #
 # site = {
 #     'html': {
@@ -164,17 +167,55 @@
 # print("Итоговый ответ — ", result)
 
 
-def sum_test(x, y):
-    return x + y
+# def sum_test(x, y):
+#     return x + y
+#
+#
+# def test_return(a, b, c):
+#     new_summ = sum_test(a, b)
+#     # — вызывая эту функцию, мы хотим получить промежуточный результат; мы не хотим, чтобы наша функция завершилась;
+#     result = new_summ + c
+#     # получив результат вложенной функции, мы его изменяем
+#     # и возвращаем
+#     return result
+#
+#
+# value = test_return(1, 2, 3)
+
+# def try_to_change_values(some_list, num):
+#     for i_index, i_val in enumerate(some_list):
+#         some_list[i_index] += 10
+#
+#
+# my_list = [1, 2, 3]
+# number = 100
+# try_to_change_values(my_list, number)
+# print(f'{my_list, number}')
+
+# Задача 1. Ошибка В одном проекте на 10 000 строк кода произошла критическая ошибка. Хорошо, что старший разработчик
+# быстро её нашёл и исправил. Он решил проверить, смогли бы вы её исправить, если бы его не было на месте. Поэтому он
+# написал для вас код с аналогичной ошибкой:
+
+# Суть кода в том, что у вас есть общий словарь из нескольких ключей, значения которых равны ранее объявленным
+# переменным. Затем вызывается функция, которая должна изменять значения словаря, добавляя к значениям случайное
+# число, в зависимости от типа данных. Но при этом меняются и ранее объявленные переменные. Исправьте эту ошибку и
+# убедитесь, что nums_list, some_dict и uniq_nums не меняются.
 
 
-def test_return(a, b, c):
-    new_summ = sum_test(a, b)
-    # — вызывая эту функцию, мы хотим получить промежуточный результат; мы не хотим, чтобы наша функция завершилась;
-    result = new_summ + c
-    # получив результат вложенной функции, мы его изменяем
-    # и возвращаем
-    return result
+def change_dict(dct):
+    num = random.randint(1, 100)
+    for i_key, i_value in dct.items():
+        if isinstance(i_value, list):
+            i_value.append(num)
+        if isinstance(i_value, dict):
+            i_value[num] = i_key
+        if isinstance(i_value, set):
+            i_value.add(num)
 
 
-value = test_return(1, 2, 3)
+nums_list = [1, 2, 3]
+some_dict = {1: 'text', 2: 'another text'}
+uniq_nums = {1, 2, 3}
+common_dict = {1: nums_list, 2: some_dict, 3: uniq_nums, 4: (10, 20, 30)}
+change_dict(common_dict)
+print(common_dict)

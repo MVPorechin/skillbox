94ac2828995b4f5e962509560c93c842f9114a0b
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-02-05 22:52:13 +0300

    draft m20_v0.03

--- draft_m21.py
+++ draft_m21.py
@@ -1,4 +1,5 @@
 import random
+import copy
 ## home work
 # def shortest_seq_range(string, tpl):
 #     return min(len(string), len(tpl))
@@ -202,20 +203,88 @@ import random
 # убедитесь, что nums_list, some_dict и uniq_nums не меняются.
 
 
-def change_dict(dct):
-    num = random.randint(1, 100)
-    for i_key, i_value in dct.items():
-        if isinstance(i_value, list):
-            i_value.append(num)
-        if isinstance(i_value, dict):
-            i_value[num] = i_key
-        if isinstance(i_value, set):
-            i_value.add(num)
+# def change_dict(dct):
+#     num = random.randint(1, 100)
+#     for i_key, i_value in dct.items():
+#         if isinstance(i_value, list):
+#             i_value.append(num)
+#         if isinstance(i_value, dict):
+#             i_value[num] = i_key
+#         if isinstance(i_value, set):
+#             i_value.add(num)
+#
+#
+# nums_list = [1, 2, 3]
+# some_dict = {1: 'text', 2: 'another text'}
+# uniq_nums = {1, 2, 3}
+# # common_dict = {1: nums_list, 2: some_dict, 3: uniq_nums, 4: (10, 20, 30)}
+# common_dict = {1: nums_list.copy(), 2: some_dict.copy(), 3: uniq_nums.copy(), 4: (10, 20, 30)}
+# # common_dict_2 = copy.deepcopy(common_dict)
+# # Она будет особенно полезна в структурах, в которых множество вложенных переменных
+# change_dict(common_dict)
+# print(common_dict)
+
+# Задача 2. Непонятно! Друг никак не может понять эту тему с изменяемыми и неизменяемыми типами, ссылками,
+# объектами и их id. Видя, как он мучается, вы решили помочь ему и объяснить эту тему наглядно.
+# Пользователь вводит любой объект. Напишите программу, которая выводит на экран тип введённых данных, информацию о
+# его изменяемости, а также id этого объекта.
 
 
-nums_list = [1, 2, 3]
-some_dict = {1: 'text', 2: 'another text'}
-uniq_nums = {1, 2, 3}
-common_dict = {1: nums_list, 2: some_dict, 3: uniq_nums, 4: (10, 20, 30)}
-change_dict(common_dict)
-print(common_dict)
+# def check_value(val):
+#     if isinstance(input_data, str):
+#         print(f'Тип данных: str (строка)\nНеизменяемый (immutable)\nId обьекта: {id(input_data)}')
+#     if isinstance(input_data, int):
+#         print(f'Тип данных: int (целое число)\nНеизменяемый (immutable)\nId обьекта: {id(input_data)}')
+#     if isinstance(input_data, float):
+#         print(f'Тип данных: float (числа с плавающей точкой)\nНеизменяемый (immutable)\nId обьекта: {id(input_data)}')
+#     if isinstance(input_data, tuple):
+#         print(f'Тип данных: tuple (кортеж)\nНеизменяемый (immutable)\nId обьекта: {id(input_data)}')
+#     if isinstance(input_data, bool):
+#         print(f'Тип данных: bool (булево значение)\nНеизменяемый (immutable)\nId обьекта: {id(input_data)}')
+#     if isinstance(input_data, list):
+#         print(f'Тип данных: list (список)\nИзменяемый(mutable)\nId обьекта: {id(input_data)}')
+#     if isinstance(input_data, dict):
+#         print(f'Тип данных: dict (словарь)\nИзменяемый(mutable)\nId обьекта: {id(input_data)}')
+#     if isinstance(input_data, set):
+#         print(f'Тип данных: set (множество)\nИзменяемый(mutable)\nId обьекта: {id(input_data)}')
+#
+#
+# input_data = input('Введите данные: ')
+# check_value(input_data)
+
+
+# Ввод объекта осуществляется не через инпут (решение с инпутом можно реализовать, но для этого придётся прописывать множество проверок
+# введенной строки, чтобы можно было преобразовать её потом в нужный тип данных)
+
+# Добавим вспомогательные словари для работы (в них можно будет по необходимости добавлять типы, которых сейчас нет)
+data_names_dict = {
+    "<class 'str'>": "строка",
+    "<class 'dict'>": "словарь",
+    "<class 'list'>": "список",
+    "<class 'set'>": "множество"
+}
+
+mutable_check_helper = {
+    "mutable": ("словарь", "список", "множество")
+}
+
+
+def check_info(data):
+    type_of_data = type(data)
+    name_of_data = ""
+    if str(type_of_data) in data_names_dict:
+        name_of_data = data_names_dict[str(type_of_data)]
+
+    if name_of_data in mutable_check_helper["mutable"]:
+        property_of_data = "Изменяемый (mutable)"
+    else:
+        property_of_data = "Неизменяемый (immutable)"
+
+    print(f"Тип данных: {type_of_data} ({name_of_data})")
+    print(property_of_data)
+    print("Id объекта:", id(data))
+
+
+data_in = "привет"
+check_info(data_in)
+

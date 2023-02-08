e3dbf73735684d53cdaaf9c35499c919ec25b829
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-02-08 15:20:17 +0300

    draft m21_v0.03

--- draft_m21.py
+++ draft_m21.py
@@ -350,18 +350,50 @@ import copy
 #     lst.append(num)
 #     print(f'{lst}')
 
-
-def add_num(num,
-            lst=None):
-
-    lst.append(num)
-    print(f'{lst}')
-
-
-add_num(5)
-add_num(10)
-add_num(15)
-
-
 # И посмотрите, что произойдёт.
 # После этого сделайте значение lst по умолчанию None и поправьте функцию, чтобы она работала правильно.
+
+# def add_num(num,
+#             lst=None):
+#     lst = lst or []
+#     # if not lst:
+#     #     lst = []
+#     lst.append(num)
+#     print(f'{lst}')
+#
+#
+# add_num(5)
+# add_num(10)
+# add_num(15)
+
+# Задача 3. Помощь другу Нашего друга попросили написать функцию, которая на вход принимает список всякого мусора.
+# Ему нужно подготовить из этого списка список словарей, чтобы его коллеги смогли дальше продолжить обработку данных.
+# Вот список правил, что нужно сделать с изначальным списком: Если в списке встретился словарь, то оставляем его.
+# Если в списке встретилась строка, то из неё нужно сделать словарь и положить его в итоговый список, например  “abc”
+# → {“abc”: “abc”}. С числами нужно сделать то же самое, что и со строками. Всё остальное выкидываем из нашего списка.
+# Друг написал программу, но в ней ошибка, так как она что-то не то выводит:( Нужна ваша помощь, вот сама программа:
+
+
+def create_dict(data, template=dict()):
+    if isinstance(data, dict):
+        return data
+
+    if isinstance(data, int) \
+            or isinstance(data, float) \
+            or isinstance(data, str):
+        template[data] = data
+        return template
+
+
+def data_preparation(old_list):
+    new_list = []
+
+    for i_element in old_list:
+        new_list.append(create_dict(i_element))
+
+    return new_list
+
+
+data = ['sad', {'sds': 23}, {43}, [12, 42, 1], 2323]
+data = data_preparation(data)
+print(data)

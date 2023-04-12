f9f9c8950d20deffbaf7a5537946a50129b6faf8
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-04-12 17:43:02 +0300

    draft m23 edu

--- m23/draft.py
+++ m23/draft.py
@@ -1,53 +1,56 @@
-def divide(number):
-    return 10 / number
+import os
 
-
-def sum_of_diveded(left, right):
-    div_sum = 0
-    for i_num in range(left, right + 1):
-        try:
-            div_sum += divide(i_num)
-            print(div_sum)
-        except ZeroDivisionError as exc:
-            print(f'{exc, type(exc)} - на ноль делить нельзя')
-    return div_sum
-
-
-total = 0
-try:
-    number_file = open('numbers.txt', 'r')
-    for i_line in number_file:
-        nums_list = i_line.split()
-        first_num = int(nums_list[0])
-        second_num = int(nums_list[1])
-
-        total += sum_of_diveded(first_num, second_num)
-    print(f'Общая сумма {total}')
-
-except ZeroDivisionError as exc:
-    print(f'{exc, type(exc)} - на ноль делить нельзя')
-except FileNotFoundError as exc:
-    print(f'{exc, type(exc)} - Такого файла или каталога нет')
+#
+# def divide(number):
+#     return 10 / number
+#
+#
+# def sum_of_diveded(left, right):
+#     div_sum = 0
+#     for i_num in range(left, right + 1):
+#         try:
+#             div_sum += divide(i_num)
+#             print(div_sum)
+#         except ZeroDivisionError as exc:
+#             print(f'{exc, type(exc)} - на ноль делить нельзя')
+#     return div_sum
+#
+#
+# total = 0
+# try:
+#     number_file = open('numbers.txt', 'r')
+#     for i_line in number_file:
+#         nums_list = i_line.split()
+#         first_num = int(nums_list[0])
+#         second_num = int(nums_list[1])
+#
+#         total += sum_of_diveded(first_num, second_num)
+#     print(f'Общая сумма {total}')
+#
+# except ZeroDivisionError as exc:
+#     print(f'{exc, type(exc)} - на ноль делить нельзя')
+# except FileNotFoundError as exc:
+#     print(f'{exc, type(exc)} - Такого файла или каталога нет')
 
 #Задача 1. Пятый элемент
 
 # В курсе по программированию студенту дали простую задачу: умножить константу (число 42) на пятый элемент строки,
 # введённой пользователем. Вот код студента:
 
-BRUCE_WILLIS = 42
-input_data = input('Введите строку: ')
-
-try:
-    leeloo = int(input_data[4])
-    result = BRUCE_WILLIS * leeloo
-    print(f'Leeloo Dallas! Multi-pass № {result}!')
-except ValueError as exc:  # as - этот оператор запишет пойманное исключение в переменную exc (название может быть
-    # любым)
-    print(f'{exc, type(exc)} — невозможно преобразовать к числу')
-except IndexError as exc:
-    print(f'{exc, type(exc)} — выход за границы списка')
-except Exception as exc:
-    print(f'{exc, type(exc)} - "поймано исключение')
+# BRUCE_WILLIS = 42
+# input_data = input('Введите строку: ')
+#
+# try:
+#     leeloo = int(input_data[4])
+#     result = BRUCE_WILLIS * leeloo
+#     print(f'Leeloo Dallas! Multi-pass № {result}!')
+# except ValueError as exc:  # as - этот оператор запишет пойманное исключение в переменную exc (название может быть
+#     # любым)
+#     print(f'{exc, type(exc)} — невозможно преобразовать к числу')
+# except IndexError as exc:
+#     print(f'{exc, type(exc)} — выход за границы списка')
+# except Exception as exc:
+#     print(f'{exc, type(exc)} - "поймано исключение')
 
 
 #Задача 2. Возраст
@@ -59,32 +62,86 @@ except Exception as exc:
 # но это оказалась директория. Неверный тип данных и некорректное значение (две ошибки обрабатываются одинаково). При
 # желании воспользуйтесь подсказкой, чтобы найти подходящие имена ошибок.
 
-file_ages = None
-file_result = None
-
-try:
-    file_ages = open("ages.txt", "r", encoding="utf8")
-    file_result = open("result.txt", "x", encoding="utf8")
-    # режим 'x' - это эксклюзивное создание, бросается исключение FileExistsError, если файл уже существует.
-except (FileExistsError, PermissionError) as exc:  # названия исключений можно группировать в кортежи
-    print(f'{exc, type(exc)} - Поймано исключение ')
-except FileNotFoundError as exc:
-    print(f'{exc, type(exc)} - Файл не найден ')
-
-if file_result and file_ages:
-    names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-    count = 0
-    for line in file_ages:
-        try:
-            clear_line = line.rstrip()
-            int(clear_line)  # на всякий случай пытаемся преобразить данные в int, но не сохраняем это в переменную, т.к. записывать нам
-            # в файл придётся именно строку
-            file_result.write(names[count] + ' - ' + clear_line + '\n')
-            count += 1
-        except (ValueError, TypeError) as exc:
-            print(f'{exc, type(exc)} - Поймано исключение ')
+# file_ages = None
+# file_result = None
+#
+# try:
+#     file_ages = open("ages.txt", "r", encoding="utf8")
+#     file_result = open("result.txt", "x", encoding="utf8")
+#     # режим 'x' - это эксклюзивное создание, бросается исключение FileExistsError, если файл уже существует.
+# except (FileExistsError, PermissionError) as exc:  # названия исключений можно группировать в кортежи
+#     print(f'{exc, type(exc)} - Поймано исключение ')
+# except FileNotFoundError as exc:
+#     print(f'{exc, type(exc)} - Файл не найден ')
+#
+# if file_result and file_ages:
+#     names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+#     count = 0
+#     for line in file_ages:
+#         try:
+#             clear_line = line.rstrip()
+#             int(clear_line)  # на всякий случай пытаемся преобразить данные в int, но не сохраняем это в переменную, т.к. записывать нам
+#             # в файл придётся именно строку
+#             file_result.write(names[count] + ' - ' + clear_line + '\n')
+#             count += 1
+#         except (ValueError, TypeError) as exc:
+#             print(f'{exc, type(exc)} - Поймано исключение ')
 
 
 # Задача 1. Простая программа Напишите программу, которая открывает файл и записывает туда введённую пользователем
 # строку. Используйте операторы try except else finally. Обработайте возможные ошибки: Проблема при открытии файла.
 # Нельзя преобразовать данные в целое. Неожиданная ошибка.
+
+file = None
+try:
+    file = open("23.3.txt", "w", encoding="utf8")
+    number = int(input("Введите текст: "))
+    file.write(str(number))
+except (FileNotFoundError, PermissionError) as exc:
+    print(type(exc), exc)
+except ValueError as exc:
+    print(type(exc), exc)
+except Exception as exc:
+    print(type(exc), exc)
+else:
+    print("Запись прошла без ошибок")
+finally:
+    if file and not file.closed:
+        file.close()
+
+
+# Задача 2. Старая задача
+#
+# Возьмите любую задачу из практической работы, например, предыдущего модуля и оформите её решение
+# в виде try except finally (можно ещё и else), обрабатывая возможные ошибки.
+#
+# Посмотрев на то, что получилось, попробуйте ответить себе на такой вопрос:
+# когда стоит использовать обработку исключений и когда она будет излишней?
+
+
+def find_file(cur_path, file_name):
+    for i_elem in os.listdir(cur_path):
+        path = os.path.join(cur_path, i_elem)
+        if file_name == i_elem:
+            print(os.path.abspath(path))
+        elif os.path.isdir(path):
+            result = find_file(path, file_name)
+            if result:
+                break
+    else:
+        result = None
+
+    return result
+
+
+try:
+    find_file('c:\\', 'hel')
+except (TypeError, PermissionError) as exc:
+    print(exc, type(exc))
+
+# Хоть проверка исключений и является мощным инструментом, НО отдавать предпочтение нужно простым проверкам (избежать исключения лучше)
+# Если же мы не уверены в появлении ошибки (например у нас нет власти над источником ошибки - это может быть какой-то сторонний сервис)
+# И если простая проверка не поможет учесть все варианты - мы можем использовать исключения.
+# НО даже в этом случае, старайтесь разделять исключения, которые вы выбрасываете специально и исключения которые могут появиться случайно.
+# Причина такой осторожности в том, что мы можем запутаться и поймать например исключение, о котором мы не догадывались. А если мы его
+# поймаем и не увидим - мы можем пропустить ошибку в нашем коде.

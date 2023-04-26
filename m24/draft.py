8f625a941aeab52fc3ff5af831ebbf41cd48a215
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-04-26 16:34:16 +0300

    draft m24 edu

--- m24/draft.py
+++ m24/draft.py
@@ -1,22 +1,22 @@
 import random
 
-class User:
-    user_name = 'Admin'
-    password = 'querty'
-    is_banned = False
-
-
-user_1 = User()  # экземлпяр класса User
-
-# user_1.user_name = 'Tom'
-# print(user_1.user_name)
+# class User:
+#     user_name = 'Admin'
+#     password = 'querty'
+#     is_banned = False
+#
+#
+# user_1 = User()  # экземлпяр класса User
+#
+# # user_1.user_name = 'Tom'
+# # print(user_1.user_name)
+# # user_2 = User()
+# # print(user_2.user_name)
 # user_2 = User()
-# print(user_2.user_name)
-user_2 = User()
-user_2.user_name = 'Tom'
-print(user_1.user_name, user_2.user_name)
-User.user_name = 'noname'
-print(user_1.user_name, user_2.user_name)
+# user_2.user_name = 'Tom'
+# print(user_1.user_name, user_2.user_name)
+# User.user_name = 'noname'
+# print(user_1.user_name, user_2.user_name)
 
 
 # Задача 1. Машина
@@ -34,13 +34,11 @@ class Toyota:
     cur_speed = 0
 
 
-car_1 = Toyota()
-car_2 = Toyota()
-car_3 = Toyota()
-car_1.cur_speed = random.randint(0, 200)
-car_2.cur_speed = random.randint(0, 200)
-car_3.cur_speed = random.randint(0, 200)
-print(car_1.cur_speed, car_2.cur_speed, car_3.cur_speed)
+cars = [Toyota() for _ in range(3)]
+for index, number in enumerate(cars):
+    cars[index].cur_speed = random.randint(0, 200)
+
+# print(cars[0].cur_speed, cars[1].cur_speed, cars[2].cur_speed)
 
 # Задача 2. Однотипные объекты В офис заказали небольшую партию из четырёх мониторов и трёх наушников. У монитора
 # есть четыре характеристики: название производителя, матрица, разрешение и частота обновления экрана. Все четыре
@@ -71,3 +69,65 @@ for index, number in enumerate([60, 144, 70, 50]):
 headphones[0].headphones_micro = False
 
 
+class User:
+    user_name = 'Admin'
+    password = 'querty'
+    is_banned = False
+    friends = []
+
+    def print_info(self):
+        print('Name:{}\nPassword: {}\nBan Status: {}\n'.format(
+            self.user_name, self.password, self.is_banned)
+        )
+
+    def add_friend(self, friend):
+        if isinstance(friend, User):
+            self.friends.append(friend.user_name)
+        else:
+            self.friends.append(friend)
+
+
+user_1 = User()  # экземлпяр класса User
+# user_1.print_info()
+user_1.add_friend('ivan')
+user_2 = User()
+user_2.user_name = 'Natasha'
+user_1.add_friend(user_2)
+# print(user_1.friends)
+
+
+class Family:
+    surname = 'Common family'
+    money = 100000
+    have_a_house = False
+
+    def info(self):
+        print('Family name: {}\nFamily funds: {}\nHaving a house: {}\n'.format(
+            self.surname, self.money, self.have_a_house
+            )
+        )
+
+    def earn_money(self, amount):
+        self.money += amount
+        print('Earned {} money! Currient value: {}'.format(amount, self.money))
+
+    def buy_house(self, house_price, discount=0):
+        house_price -= house_price * discount / 100
+        if self.money >= house_price:
+            self.money -= house_price
+            self.have_a_house = True
+            print('House purchased! Currient money: {}\n'.format(self.money))
+        else:
+            print('Not enough money!\n')
+
+
+my_family = Family()
+my_family.info()
+print('try to buy a house')
+my_family.buy_house(10**6)
+if not my_family.have_a_house:
+    my_family.earn_money(800000)
+    print('Try to buy a house again')
+    my_family.buy_house(10 ** 6, 10)
+
+my_family.info()

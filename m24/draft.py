88061bbabc1ed1f69018cbb84f4b50c671fc833c
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-04-28 15:48:08 +0300

    draft m24 edu

--- m24/draft.py
+++ m24/draft.py
@@ -1,3 +1,4 @@
+from garden import PotapoGarden
 import random
 
 
@@ -243,41 +244,6 @@ dot_2.info()
 # Грядка с картошкой содержит список картошки, которая на ней растёт, и может, собственно, взращивать всю эту картошку, а также предоставлять информацию о зрелости всей картошки на своей территории.
 
 # Веселая ферма
-class Potapo:
-    states = {0: 'Отсутствует', 1: 'Росток', 2: 'Зеленая', 3: 'Зрелая'}
-
-    def __init__(self, index):
-        self.index = index
-        self.state = 0
-
-    def grow(self):
-        if self.state < 3:
-            self.state += 1
-        self.print_state()
-
-    def is_ripe(self):
-        if self.state == 3:
-            return True
-        return False
-
-    def print_state(self):
-        print(f'Картошка {index} в стадии {Potapo.states[self.state]}')
-
-
-class PotapoGarden:
-    def __init__(self, count):
-        self.potatoes = [Potapo(index) for index in range(1, count + 1)]
-
-    def grow_all(self):
-        print(f'картошка прорастает!')
-        for i_potapo in self.potatoes:
-            i_potapo.grow()
-
-    def are_all_ripe(self):
-        if not all([i_potapo.is_ripe() for i_potapo in self.potatoes]):
-            print('Картошка еще не созрела!\n')
-        else:
-            print('Вся картошка созрела! можно собирать\n')
 
 
 my_garden = PotapoGarden(5)
@@ -285,3 +251,5 @@ my_garden.are_all_ripe()
 for _ in range(3):
     my_garden.grow_all()
     my_garden.are_all_ripe()
+
+

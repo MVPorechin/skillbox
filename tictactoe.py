81d999495104b3fbe477f9da10cab59236d09fb3
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-05-27 18:13:36 +0300

    init

--- tictactoe.py
+++ tictactoe.py
@@ -1,122 +1,112 @@
+class Cell:
+    #  Клетка, у которой есть значения
+    #   - занята она или нет
+    #   - номер клетки
+    def __init__(self, status=None, num_cell=None):
+        self.status = status
+        self.num_cell = num_cell
+
+    def __str__(self):
+        return f'{self.status}\t'
+
+
+class Board:
+    #  Класс поля, который создаёт у себя экземпляры клетки
+    def __init__(self):
+        self.board = [[Cell('*', ord_y) for ord_y in range(3)] for ord_x in range(3)]
+
+    def print_board(self):
+        for ord_y in self.board:
+            print(*ord_y)
+            # print(' '.join(map(str, ord_y)))
+            # print(*cell, sep='')
+
+    def change_value_cell(self, user, ord_y, ord_x):
+        self.board[ord_y][ord_x] = user
+
+
 class Player:
-  def __init__(self,name):
-    self.userName=name
-
-
-class Grid:
-  def __init__(self):
-    self.grid=[[0 for x in range(3)] for y in range(3)]
-
-  def printGrid(self):
-    for y in range(0,len(self.grid)):
-      for x in range(0,len(self.grid[y])):
-        print(self.grid[y][x],end=" ")
-      print("")
-
-  def isValid(self):
-    for y in range(len(self.grid)):
-      for x in range(len(self.grid[y])):
-        if(self.grid[y][x]==0):
-          return True
-    return False
-
-
-  def validMove(self,y,x):
-    return (y>=0 and y<=2) and (x>=0 and x<=2) and self.grid[y][x]==0
-
-  def setValue(self,y,x,val):
-    self.grid[y][x]=val
-
-
-  def solve(self):
-    # Horizontal Check
-    for y in range(len(self.grid)):
-      state=self.grid[y][0]
-      flag=True
-      if(state==0):
-        continue
-      for x in range(1,len(self.grid[y])):
-        if(self.grid[y][x]!=state):
-          flag=False
-          break
-      if(flag):
-        return True
-
-    # Vertical Check
-    for x in range(len(self.grid[0])):
-      state=self.grid[0][x]
-      flag=True
-      if(state==0):
-        continue
-      for y in range(1,len(self.grid)):
-        if(self.grid[y][x]!=state):
-          flag=False
-          break
-      if(flag):
-          return True
-
-    # Diagonal Check
-    if(self.grid[1][1]!=0 and self.grid[1][1]==self.grid[0][0] and self.grid[1][1]==self.grid[2][2]):
-      return True
-    if(self.grid[1][1]!=0 and self.grid[1][1]==self.grid[0][2] and self.grid[1][1]==self.grid[2][0]):
-      return True
-
-    # No One Won
-    return False
+    #  У игрока может быть
+    #   - имя
+    #   - на какую клетку ходит
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
 
 
 class Game:
-  def __init__(self,user1,user2):
-    self.player1=user1
-    self.player2=user2
-    self.choice=True
-    self.grid=Grid()
+    player_win = False
+    wall_of_fame = dict()
 
-  def gameInstance(self):
-    self.grid.printGrid()
+    # класс «Игры» содержит атрибуты:
+    # состояние игры,
+    # игроки,
+    # поле.
+    def __init__(self, condition_game, players_list, field):
+        self.condition_game = condition_game
+        self.players_list = players_list
+        self.field = field
 
-  def play(self):
-    if(not self.grid.isValid()):
-      print("MATCH DRAW")
-      return
+    # Метод запуска одного хода игры.
+    # Получает одного из игроков, запрашивает у игрока номер клетки, изменяет поле, проверяет, выиграл ли игрок.
+    # Если игрок победил, возвращает True, иначе False.
 
-    if(self.choice):
-      print("Player 1: "+self.player1.userName+" Move")
-      y1,x1=map(int,input().split())
-      y1-=1
-      x1-=1
-      if(self.grid.validMove(y1,x1)):
-        self.grid.setValue(y1,x1,1)
-        self.gameInstance()
-        if(self.grid.solve()):
-          print("Player 1: "+self.player1.userName+" Won the Match")
-          return
-        self.choice=False
-        self.play()
-      else:
-        print("Invalid Move...Try A Valid Move Instead")
-        self.play()
+    def one_step(self, player):
+        player_ord_y, player_ord_x = [int(value) for value in input(f'{player.name} делает ход: ').split()]
+        self.field.change_value_cell(player.symbol, player_ord_y, player_ord_x)
+        self.field.print_board()
+        for line in self.rows() + self.columns() + self.diagonals():
+            sym = set(line)
+            if player.symbol in sym and len(sym) == 1:
+                return True
+        else:
+            False
 
-    else:
-      print("Player 2: "+self.player2.userName+" Move")
-      y2,x2=map(int,input().split())
-      y2-=1
-      x2-=1
-      if(self.grid.validMove(y2,x2)):
-        self.grid.setValue(y2,x2,2)
-        self.gameInstance()
-        if(self.grid.solve()):
-          print("Player 2: "+self.player2.userName+" Won the Match")
-          return
-        self.choice=True
-        self.play()
-      else:
-        print("Invalid Move...Try A Valid Move Instead")
-        self.play()
+    def rows(self):
+        return self.field.board[:]
 
+    def columns(self):
+        return [[row[index] for row in self.field.board] for index in range(3)]
 
-player1=Player(input("Enter first player's name : "))
-player2=Player(input("Enter second player's name : "))
-newGame=Game(player1,player2)
-newGame.gameInstance()
-newGame.play()
+    def diagonals(self):
+        backslash = [self.field.board[index][index] for index in range(3)]
+        slash = [self.field.board[index][len(self.field.board[index]) - index - 1] for index in range(3)]
+        return [backslash, slash]
+
+    def cleanboard(self):
+        return [[self.field.change_value_cell('*', ord_y, ord_x) for ord_y in range(3)] for ord_x in range(3)]
+
+    def currient_session(self):
+        self.field.print_board()
+        while not self.condition_game:
+            for player in self.players_list:
+                if self.one_step(player):
+                    print(f'{player.name} победил!')
+                    Game.wall_of_fame[player.name] = 1
+                    self.cleanboard()
+                    self.print_wall_of_fame()
+                    return True
+                    # Метод запуска одной игры.
+        # Очищает поле, запускает цикл с игрой, который завершается победой одного из игроков или ничьей.
+        # Если игра завершена, метод возвращает True, иначе False.
+        pass
+
+    def print_wall_of_fame(self):
+        return [print(f'Игрок {key}, счет: {self.wall_of_fame[key]}') for key in
+                sorted(self.wall_of_fame, key=self.wall_of_fame.get, reverse=True)]
+
+    def run_game(self):
+        self.currient_session()
+        while True:
+            ask_a_question = input('Игра завершена, запустить заново? "Да" или "Нет": ').lower()
+            if ask_a_question == 'да':
+                self.currient_session()
+                print('Доска победы')
+                # self.print_wall_of_fame()
+                return True
+            else:
+                break
+
+    # Основной метод запуска игр.
+    # В цикле запускает игры, запрашивая после каждой игры, хотят ли игроки продолжать играть.
+    # После каждой игры выводится текущий счёт игроков.

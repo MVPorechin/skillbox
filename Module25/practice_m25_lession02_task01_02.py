b6d330d83153cfbbec7d7b14832d81682b0a183a
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-05-31 22:26:58 +0300

    add practice_m25_lession02.py lession 02 module 25 task 01 and 02

--- Module25/practice_m25_lession02_task01_02.py
+++ Module25/practice_m25_lession02_task01_02.py
@@ -56,10 +56,58 @@ print(new_dot)
 # Задача 2. Человек
 #
 # Реализуйте класс «Человек», который инициализируется именем (имя должно состоять только из букв)
-# и возрастом (должен быть в диапазоне от 0 до 100), а внутри класса считается общее количество инициализированных объектов.
+# и возрастом (должен быть в диапазоне от 0 до 100),
+# а внутри класса считается общее количество инициализированных объектов.
 # Реализуйте сокрытие данных для всех атрибутов (как статических, так и динамических),
 # а для изменения и получения данных объекта напишите специальные геттеры и сеттеры.
 #
 # При тестировании класса измените приватный атрибут (например, возраст) двумя способами:
 # сеттером и «крайне не рекомендуемым», который был показан в видео.
 
+class Human:
+    __count = 0
+
+    def __init__(self, name, age):
+        self.__name = ''
+        self.__age = 0
+        self.set_name(name)
+        self.set_age(age)
+
+        Human.__count += 1
+
+    def __str__(self):
+        return f"Имя: {self.__name}\tВозраст: {self.__age} лет"
+
+    def set_name(self, value):
+        if isinstance(value, str) and value.isalpha():
+            self.__name = value
+        else:
+            raise ValueError('Ошибка в имени')
+
+    def set_age(self, value):
+        if isinstance(value, (int, float)) and 0 <= value <= 100:
+            self.__age = value
+        else:
+            raise ValueError('Ошибка в возрасте')
+
+    def get_count(self):
+        return self.__count
+
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+
+anna = Human('Анна', 19)  # значения передадутся в сеттер
+ruzanna = Human('Рузанна', 18)  # значения передадутся в сеттер
+print(anna.get_count())
+print(anna, '\n', ruzanna)
+anna.set_age(21)
+print(anna, '\n', ruzanna)
+anna.set_name('Сюзанна')
+print(anna, '\n', ruzanna)
+
+anna._Human__age = 99 # «крайне не рекомендуемый» метод
+print(anna)

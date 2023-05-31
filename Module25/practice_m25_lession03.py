921069192ec22cff7e8e4f3f84a2616e129bb10b
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-06-01 01:39:03 +0300

    add practice_m25_lession02.py lession 03 module 25

---
+++ Module25/practice_m25_lession03.py
@@ -0,0 +1,28 @@
+class Pet:
+    legs = 4
+    has_tail = True
+
+    def __init__(self):
+        tail = 'да' if self.has_tail else 'нет'
+        return 'Всего ног: {legs}\nХвост присутствует - {has_tail}'.format(
+            legs=self.legs,
+            has_tail=tail
+        )
+
+
+class Cat(Pet):
+    def sound(self):
+        print('Мяу!')
+
+
+class Dog(Pet):
+    def sound(self):
+        print('Гав!')
+
+
+cat = Cat()
+dog = Dog()
+print(cat)
+print(dog)
+cat.sound()
+dog.sound()

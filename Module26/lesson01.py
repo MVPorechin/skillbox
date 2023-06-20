ae863c185b94d5d78d9f777b26057b158f2ccb66
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-06-20 23:34:49 +0300

    add practice_m26 add lession 01

--- Module26/lesson01.py
+++ Module26/lesson01.py
@@ -1,3 +1,5 @@
+import random
+
 class MyIter:
     """
 
@@ -26,3 +28,38 @@ class MyIter:
 
 my_list = [1, 2, 3, 4, 5]
 
+
+iterator = my_list.__iter__()
+end_iter = False
+
+while not end_iter:
+    try:
+        step_iter = iterator.__next__()
+        print(step_iter)
+    except StopIteration as exc:
+        end_iter = True
+        print(type(exc), exc)
+
+# print(iterator.__next__())
+# print(iterator.__next__())
+# print(iterator.__next__())
+# print(next(iterator))
+# print(next(iterator))
+
+# Задача 1. Свой for (ну почти)
+#
+# Дан любой итерируемый объект, например список из N чисел. Реализуйте функцию,
+# которая эмулирует работу цикла for с помощью цикла while и проходит во всем элементам итерируемого объекта.
+# Не забудьте про исключение «конца итерации».
+
+n = int(input("Введите количество чисел: "))
+numbers = [random.randint(-100, 100) for _ in range(n)]
+
+buffer_iter = iter(numbers)
+
+while True:
+    try:
+        print(next(buffer_iter))
+    except StopIteration:
+        print("Конец!")
+        break

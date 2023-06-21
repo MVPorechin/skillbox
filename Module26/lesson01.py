4afd73f022bf8b9fe72baba5dd8602b8af57421f
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-06-21 18:14:11 +0300

    add practice_m25

--- Module26/lesson01.py
+++ Module26/lesson01.py
@@ -1,33 +1,32 @@
 import random
 
-class MyIter:
-    """
 
-    """
-
-    def __init__(self, iterable):
-        """
-
-        :param iter_object:
-        """
-        self.iterable = iterable
-        self.end_iter_object = False
-
-
-    def loop(self, loop_body_func):
-        self.iterator = iter(self.iterable)
-
-        while not self.end_iter_object:
-            try:
-                elem_from_iterator = next(self.iterator)
-            except StopIteration:
-                self.end_iter_object = False
-            else:
-                loop_body_func(elem_from_iterator)
+# class MyIter:
+#     """
+#
+#     """
+# 
+#     def __init__(self, iterable):
+#         """
+#
+#         :param iter_object:
+#         """
+#         self.iterable = iterable
+#         self.end_iter_object = False
+#
+#     def loop(self, loop_body_func):
+#         self.iterator = iter(self.iterable)
+#
+#         while not self.end_iter_object:
+#             try:
+#                 elem_from_iterator = next(self.iterator)
+#             except StopIteration:
+#                 self.end_iter_object = False
+#             else:
+#                 loop_body_func(elem_from_iterator)
 
 
 my_list = [1, 2, 3, 4, 5]
-
 
 iterator = my_list.__iter__()
 end_iter = False

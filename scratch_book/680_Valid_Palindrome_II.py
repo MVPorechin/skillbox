46c7bf368bbcf2eda27894eb8615a24f6fe5f8ac
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-07-03 16:09:44 +0300

    add task palindrome II

--- scratch_book/680_Valid_Palindrome_II.py
+++ scratch_book/680_Valid_Palindrome_II.py
@@ -12,12 +12,15 @@ class Solution(object):
                 left_index += 1
             while not (s[right_index].isdigit() or s[right_index].isalpha()) and left_index < right_index:
                 right_index -= 1
-            if s[left_index].lower() != s[right_index].lower():
-                return False
+            if s[left_index] != s[right_index]:
+                if (s[left_index + 1] == s[right_index - 1]) or (s[left_index + 1] == s[right_index]) or s[left_index] == s[right_index - 1]:
+                    return True
+                else:
+                    return False
             left_index += 1
             right_index -= 1
         return True
 
 
 my_sol = Solution()
-print(my_sol.validPalindrome(s='abca'))
+print(my_sol.validPalindrome(s='EECCCCBEBAEEABEBCCCEEA'))

a48999aa884cb9f7a81b3bf450a4bb00f9a9d055
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-07-03 17:49:19 +0300

    add task palindrome II

--- scratch_book/680_Valid_Palindrome_II.py
+++ scratch_book/680_Valid_Palindrome_II.py
@@ -1,4 +1,15 @@
 class Solution(object):
+    def is_palindrome(self, s):
+        left_index = 0
+        right_index = len(s) - 1
+        while left_index < right_index:
+            if s[left_index] == s[right_index]:
+                left_index += 1
+                right_index -= 1
+            else:
+                return False
+        return True
+
     def validPalindrome(self, s):
         """
         :type s: str
@@ -6,19 +17,12 @@ class Solution(object):
         """
         left_index = 0
         right_index = len(s) - 1
-
         while left_index < right_index:
-            while not (s[left_index].isdigit() or s[left_index].isalpha()) and left_index < right_index:
+            if s[left_index] == s[right_index]:
                 left_index += 1
-            while not (s[right_index].isdigit() or s[right_index].isalpha()) and left_index < right_index:
                 right_index -= 1
-            if s[left_index] != s[right_index]:
-                if (s[left_index + 1] == s[right_index - 1]) or (s[left_index + 1] == s[right_index]) or s[left_index] == s[right_index - 1]:
-                    return True
-                else:
-                    return False
-            left_index += 1
-            right_index -= 1
+            else:
+                return self.is_palindrome(s[left_index + 1:right_index+1]) or self.is_palindrome(s[left_index:right_index])
         return True
 
 

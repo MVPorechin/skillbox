5b1ce85238b8f87b55d335d9c7b73e5372894886
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-07-09 18:49:43 +0300

    add 26. Remove Duplicates from Sorted Array

---
+++ scratch_book/26_Remove Duplicates from Sorted Array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeDuplicates(self, nums: list[int]) -> int:
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left_point = 1
+        for right_point in range(1, len(nums)):
+            if nums[right_point] != nums[right_point - 1]:
+                nums[left_point] = nums[right_point]
+                left_point += 1
+        return left_point
+
+
+my_sol = Solution()
+value = my_sol.removeDuplicates(nums=[0,0,1,1,1,2,2,3,3,4])
+print(value)

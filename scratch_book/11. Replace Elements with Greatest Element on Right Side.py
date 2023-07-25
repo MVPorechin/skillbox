253b18240652cbc84407fd59d15b17b76d8e09ae
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-07-25 14:59:10 +0300

    add

--- scratch_book/11. Replace Elements with Greatest Element on Right Side.py
+++ scratch_book/11. Replace Elements with Greatest Element on Right Side.py
@@ -4,27 +4,20 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        left, right = len(arr) - 2, len(arr) - 1
-        max_value = 0
-        while left > 0:
-            for index in range(len(arr), 0, -1):
-                if len(arr) == 1:
-                    arr[-1] = -1
-                elif arr[index - 1] > max_value:
-                    max_value = arr[index - 1]
-                elif arr[index - 1] < max_value:
-                    arr[index - 1] = max_value
-                elif arr[right] <= arr[left]:
-                    arr[left] = arr[right]
-                    right -= 1
+        max_value = arr[-1]
+        for index in range(len(arr), 0, -1):
 
-                left -= 1
+            if arr[index-1] > max_value:
+                max_value = arr[index - 1]
+            elif arr[index-1] < max_value:
+                arr[index-1] = max_value
+
         arr.remove(arr[0])
         arr.append(-1)
         return arr
 
 
 my_sol = Solution()
-my_sol.replaceElements(arr=[17,18,5,4,6,1])
+rt=my_sol.replaceElements(arr=[17,18,5,4,6,1])
 
-print(my_sol)
+print(rt)

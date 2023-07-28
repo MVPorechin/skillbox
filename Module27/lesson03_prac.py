7673a7988080687f9483d873598123b5fbf53857
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-07-28 15:35:01 +0300

    add module 27, practice decarator add new practice

--- Module27/lesson03_prac.py
+++ Module27/lesson03_prac.py
@@ -70,3 +70,20 @@ def say_goodbye(name: str) -> str:
 
 print(PLUGINS)
 print(say_hello('Tom'))
+
+
+
+
+plugins = {}
+
+
+def go_to_plugins(func):
+    plugins[func.__name__] = func
+    return func
+
+
+@go_to_plugins
+def hello():
+    print('Hello!')
+
+print(plugins)

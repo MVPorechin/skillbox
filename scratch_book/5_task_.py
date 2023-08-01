5d0da3a0b0763562aee9e9616145fc19c1b8f439
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-08-02 01:34:13 +0300

    task 05

--- scratch_book/5_task_.py
+++ scratch_book/5_task_.py
@@ -1,1 +1,3 @@
 #https://telegra.ph/5-zadanie-07-11
+def simple_or_not(first:int, second:int) -> int:
+    ##для каждого разряда: 1-9, 10-99, 100-999 и тд. всегда будем иметь только 9 подходящих нам чисел

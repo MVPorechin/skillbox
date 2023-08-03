72219f809be8c68d17d480595f9741fc90757e11
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-08-04 01:52:41 +0300

    task 05

--- scratch_book/5_task_.py
+++ scratch_book/5_task_.py
@@ -1,33 +1,49 @@
-from decimal import *
-
-
 # https://telegra.ph/5-zadanie-07-11
 
 def increase_by_ten(step: int = 1, lenght: int = 1) -> int:
+    """
+    Функция создает шаг для счетчика
+    на входе получаем количество разрядов создания шага
+    на выходе число (шаг)
+    :param step:
+    :param lenght:
+    :return:
+    """
     if lenght == 1:
         return step
     else:
         lenght -= 1
-        return increase_by_ten(step=step * 10 + 1)
+        return increase_by_ten(step=step * 10 + 1, lenght=lenght)
 
 
-# def simple_or_not(first: int, second: int) -> int:
-#     """для каждого разряда: 1-9, 10-99, 100-999 и тд. всегда будем иметь только 9 подходящих нам чисел"""
-#     count = 0
-#     if len(str(first)) != len(str(second)):
-#         increase_first_step = increase_by_ten(lenght=len(str(first)))
-#         increase_second_step = increase_by_ten(lenght=len(str(second)))
-#     else:
-#         increase_step = len(str(first))
-#
-#     step = increase_by_ten(lenght=increase_step)
-#
-#     for _ in range(first,second, step):
-#         count += 1
-#
-#     return count
-#
-#
-# result = simple_or_not(first=123, second=678)
-step_ = increase_by_ten(lenght=3)
-print(step_)
+def count(start: int, end: int, step: int) -> int:
+    """
+    Функция-счетчик через генератор
+    на входе получаем границы старта, конца и шага,
+    на выходе количество шагов
+    """
+    cnt = 0
+    for _ in range(start, end, step):
+        cnt += 1
+    return cnt
+
+
+def simple_or_not(left: int, right: int) -> int:
+    """Для каждого разряда: 1-9, 10-99, 100-999 и тд. Всегда будем иметь только 9 подходящих нам чисел"""
+    if len(str(right)) - len(str(left)) > 91:
+        increase_first_step = increase_by_ten(lenght=len(str(left)))
+        increase_second_step = increase_by_ten(lenght=len(str(right)))
+        first_edge = 9 * increase_first_step
+        second_edge = 9 * increase_second_step
+        first_r = count(start=left, end=first_edge, step=increase_first_step)
+        second_r = count(start=first_edge, end=right, step=increase_second_step)
+
+        return first_r + second_r
+
+    else:
+        return count(start=left, end=right, step=increase_by_ten(lenght=len(str(left))))
+
+
+l, r = map(int, input().split())
+result = simple_or_not(left=l, right=r)
+print(result)

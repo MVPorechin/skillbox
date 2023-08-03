92db35d3a5863de63acbd0de395169861c80514a
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-08-03 16:38:36 +0300

    task 05 initial

--- scratch_book/5_task_.py
+++ scratch_book/5_task_.py
@@ -1,3 +1,17 @@
-#https://telegra.ph/5-zadanie-07-11
-def simple_or_not(first:int, second:int) -> int:
-    ##для каждого разряда: 1-9, 10-99, 100-999 и тд. всегда будем иметь только 9 подходящих нам чисел
+from decimal import *
+# https://telegra.ph/5-zadanie-07-11
+def simple_or_not(first: int, second: int) -> int:
+    """для каждого разряда: 1-9, 10-99, 100-999 и тд. всегда будем иметь только 9 подходящих нам чисел"""
+    count = 0
+    if first < 10:
+        step = 1
+    else:
+        step = 10
+    for _ in range(first, second, step):
+        count += 1
+
+    return count
+
+
+result = simple_or_not(first=123, second=678)
+print(result)

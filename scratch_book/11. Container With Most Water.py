42a512f97ca41d1940c5480ab85f4e693c77d8fc
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-08-07 01:55:26 +0300

    add task 11 leetcode

--- scratch_book/11. Container With Most Water.py
+++ scratch_book/11. Container With Most Water.py
@@ -1,3 +1,12 @@
 #https://leetcode.com/problems/container-with-most-water/
+from typing import List
+
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        buy_price = height[0]
+        for sell_price in height[1:]:
+            buy_price = min(buy_price, sell_price)
+            profit = sell_price - buy_price
+            if profit > best_profit:
+                best_profit = profit

d63fe4ec48764d487d2eb84470cbccc4af2e2087
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-08-08 17:37:21 +0300

    add split_file

--- scratch_book/5_task_.py
+++ scratch_book/5_task_.py
@@ -1,19 +1,19 @@
 #https://telegra.ph/5-zadanie-07-11
 
-def increase_by_ten(step: int = 1, lenght: int = 1) -> int:
+def increase_by_ten(step: int = 1, length: int = 1) -> int:
     """
     Функция создает шаг для счетчика
     на входе получаем количество разрядов создания шага
     на выходе число (шаг)
     :param step:
-    :param lenght:
+    :param length:
     :return:
     """
-    if lenght == 1:
+    if length == 1:
         return step
     else:
-        lenght -= 10
-        return increase_by_ten(step=step * 10 + 1, lenght=lenght)
+        length -= 1
+        return increase_by_ten(step=step * 10 + 1, length=length)
 
 
 def count(start: int, end: int, step: int) -> int:
@@ -30,9 +30,9 @@ def count(start: int, end: int, step: int) -> int:
 
 def simple_or_not(left: int, right: int) -> int:
     """Для каждого разряда: 1-9, 10-99, 100-999 и тд. Всегда будем иметь только 9 подходящих нам чисел"""
-    if len(str(right)) - len(str(left)) > 91:
-        increase_first_step = increase_by_ten(lenght=len(str(left)))
-        increase_second_step = increase_by_ten(lenght=len(str(right)))
+    if len(str(right)) - len(str(left)) > 99:
+        increase_first_step = increase_by_ten(length=len(str(left)))
+        increase_second_step = increase_by_ten(length=len(str(right)))
         first_edge = 9 * increase_first_step
         second_edge = 9 * increase_second_step
         first_r = count(start=left, end=first_edge, step=increase_first_step)
@@ -41,7 +41,7 @@ def simple_or_not(left: int, right: int) -> int:
         return first_r + second_r
 
     else:
-        return count(start=left, end=right, step=increase_by_ten(lenght=len(str(left))))
+        return count(start=left, end=right, step=increase_by_ten(length=len(str(left))))
 
 
 l, r = map(int, input().split())
---
+++ scratch_book/split_txt_in_one_txt.py
@@ -0,0 +1,28 @@
+import os
+
+path = "D:\soft"
+
+
+
+
+def read_from_files_write_to_file(path:str) -> str:
+    list_files = os.listdir(path)
+    one_file = 'D:\one_file.txt'
+    with open(file=one_file, mode='a', encoding='ISO-8859-1') as one_f:
+        for file in list_files:
+            abs_path = path + "\\" + file
+            one_f.writelines(str(file)+'\n')
+            with open(file=abs_path, mode='r', encoding='ISO-8859-1') as txt:
+                for line in txt:
+
+                    one_f.writelines(str(line))
+
+    return 'ok'
+
+
+read_from_files_write_to_file(path=path,)
+
+
+
+
+

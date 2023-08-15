e44392f9447d54c8f479129e09c13465766ada23
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-08-15 23:10:34 +0300

    56 task done

--- scratch_book/56. Merge Intervals.py
+++ scratch_book/56. Merge Intervals.py
@@ -4,14 +4,17 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        lst_one = intervals[0]
-        lst_two = intervals[1]
-        for lst in range(len(intervals)):
-            print(intervals[lst])
+        intervals.sort(key=lambda i: i[0])
+        out = [intervals[0]]
+        for start, end in intervals[1:]:
+            last = out[-1][1]
+            if start <= last:
+                out[-1][1] = max(last, end)
+            else:
+                out.append([start, end])
+        return out
 
 
-
-
-my_sol = Solution
+my_sol = Solution()
 my_sol.merge(intervals=[[1,3],[2,6],[8,10],[15,18]])
 print(my_sol)

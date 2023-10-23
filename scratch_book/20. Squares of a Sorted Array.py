f60c7557048dbb70204a5ab3bc5e633b57048dc0
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-10-24 01:01:12 +0300

    add 20 task leetcode

--- scratch_book/20. Squares of a Sorted Array.py
+++ scratch_book/20. Squares of a Sorted Array.py
@@ -1,12 +1,19 @@
 from typing import List
 
-
 #leetcode.com/problems/squares-of-a-sorted-array
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        not_sort_list = []
-        for n in nums:
-            not_sort_list.append(pow(n, 2))
+        left = 0
+        right = len(nums) - 1
+        sort_list = []
+        while left <= right:
+            if abs(nums[right]) > abs(nums[left]):
+                sort_list.append(nums[right] ** 2)
+                right -= 1
+            else:
+                sort_list.append(nums[left] ** 2)
+                left += 1
+        return sort_list[::-1]
 
 
 if __name__ == '__main__':
